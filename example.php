e6eff7d51946bcef92d638f6b17949e552706bb4
waqar3 <waqar3@gmail.com>
2017-06-02 08:09:12 +0100

    SQL Injection prevention example

--- example.php
+++ example.php
@@ -49,9 +49,30 @@ $result = $db->query($query);
 echo ($result) ? "<br> Successfully update" : "<br> Update query was not successfull";
 
 //delete a row in table
-$query = "delete from guestbook where id = 2";
+$query = "delete from guestbook  id = 2";
 $result = $db->query($query);
 echo ($result) ? "<br> Successfully deleted" : "<br> Delete query was not successfull";
 
+/* SQL Injection Prevention
+Use prepareed statement and bind variables
+Most common attacks can be prevented by binding variables
+Use PHP sanitize filters on user inputs
+e.g: FILTER_SANITIZE_NUMBER_INT will remove all non-int from integer inputs
+more info: 
+http://us3.php.net/manual/en/filter.filters.sanitize.php
+http://php.net/manual/en/pdo.prepared-statements.php
+*/
+
+// sanitize user id input, remove all non digit characters
+	$id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
+
+// Insert query with prepare and bind 
+	$values = array(0 => 'userx', 1 => 'guest message', 2 => date("Y-m-d H:i:s"));
+	$stmt = $db->connection->prepare("INSERT INTO guestbook (fid, user, message, DATE) VALUES (?, ?, ?, ?)");
+	if($stmt){
+// bind variables here "sss" declares 1st, 2nd & 3rd values are string, use i for integer
+	$stmt->bind_param("sss", $values[0], $values[1], $values[2]);
+	$stmt->execute();
+	}
 
 ?>
